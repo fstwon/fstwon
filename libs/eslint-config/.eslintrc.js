24057b56289db1ee7127f145a69aab86541b3620
fstwon <fstwon@gmail.com>
2025-07-02 23:33:39 +0900

    Init: 전역 config 생성, package.json, pnpm-workspace.yaml, turbo.jsonc 수정

---
+++ libs/eslint-config/.eslintrc.js
@@ -0,0 +1,36 @@
+// libs/eslint/config.js
+
+/** @type {import('eslint').Linter.Config} */
+
+export default config = {
+	root: true, // 루트 프로젝트로 설정. 상위 설정을 무시
+
+	env: {
+		browser: true, // 브라우저 전역 객체 (window 등) 허용
+		es2021: true, // 최신 ECMAScript 문법을 사용 가능하게 설정
+	},
+
+	parserOptions: {
+		ecmaVersion: 2022, // 최신 문법 해석 (예: optional chaining)
+		sourceType: 'module', // ESM 사용 (import/export)
+	},
+
+	plugins: ['react'], // 리액트 관련 규칙 추가
+
+	extends: [
+		'eslint:recommended', // ESLint의 기본 추천 규칙 사용
+		'plugin:react/recommended', // 리액트 JSX 추천 규칙 사용
+		'prettier', // Prettier와 충돌 방지 (포맷팅 관련 규칙 off)
+	],
+
+	rules: {
+		semi: ['error', 'always'], // 세미콜론 필수
+		quotes: ['error', 'single'], // 작은 따옴표 사용
+	},
+
+	settings: {
+		react: {
+			version: 'detect', // 설치된 리액트 버전을 자동 감지
+		},
+	},
+};
