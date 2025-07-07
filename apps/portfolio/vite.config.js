b2f682c8638ee1480efd07ef7cfbf440e58f8f80
fstwon <fstwon@gmail.com>
2025-07-07 22:58:11 +0900

    Init: Netlify 배포 설정 테스트

--- apps/portfolio/vite.config.js
+++ apps/portfolio/vite.config.js
@@ -1,12 +1,23 @@
 import createBaseConfig from '../../libs/vite-base-config/vite.config';
 import { defineConfig, mergeConfig } from 'vite';
+import path from 'path';
 
 export default mergeConfig(
 	createBaseConfig(),
 	defineConfig({
 		root: __dirname,
+		build: {
+			outdir: 'dist',
+			emptyOutDir: true,
+		},
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
 		server: {
 			port: 3000,
 		},
+		base: '/',
 	})
 );
