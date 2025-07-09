78fab015727039ee8551f08813e169ce7b673374
fstwon <fstwon@gmail.com>
2025-07-09 23:30:47 +0900

    Test: 포트폴리오 애니메이션 테스트 중

--- apps/portfolio/src/App.tsx
+++ apps/portfolio/src/App.tsx
@@ -1,10 +1,3 @@
-import React from 'react';
-
 export default function App() {
-	return (
-		<div>
-			<h1>Welcome to Portfolio</h1>
-			<p>This is your portfolio app running with React + TypeScript + Vite.</p>
-		</div>
-	);
+	return <div></div>;
 }
