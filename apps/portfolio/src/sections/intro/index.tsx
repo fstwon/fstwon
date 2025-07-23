c4116be06fa01e3a0e256f16c5063591c872abe0
fstwon <fstwon@gmail.com>
2025-07-23 23:30:38 +0900

    Init: 공용 스타일 환경 설정 및 스타일 작업(진행 중)

---
+++ apps/portfolio/src/sections/intro/index.tsx
@@ -0,0 +1,7 @@
+export function IntroSection() {
+	return (
+		<div className='bg-red-500 text-white p-4 rounded-xl'>
+			Tailwind CSS 적용 확인
+		</div>
+	);
+}
---
+++ libs/tailwind-config/tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+	content: [
+		'../../apps/**/*.{js,ts,jsx,tsx}',
+		'../../packages/**/*.{js,ts,jsx,tsx}',
+		'../../libs/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
+
+export default config;
