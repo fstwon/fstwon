effcf893782e3512d5aa8d419923d5802b0d0e52
fstwon <fstwon@gmail.com>
2025-07-30 00:33:03 +0900

    INIT: 공용 스타일 설정 중

--- apps/portfolio/src/main.tsx
+++ apps/portfolio/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
-import '@styles/index.css';
+import '@tailwind/index.css';
+import '@scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
