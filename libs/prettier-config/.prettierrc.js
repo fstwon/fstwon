5831388a5b1da430b62ade0d883f17972c99d937
fstwon <fstwon@gmail.com>
2025-09-17 00:15:38 +0900

    Init: icon 사용 방법 구상 중

--- libs/prettier-config/.prettierrc.js
+++ libs/prettier-config/.prettierrc.js
@@ -1,21 +1,23 @@
 /** @type {import("prettier").Config} */
 
-export default config = {
+const config = {
   // 문장 끝에 세미콜론 사용
-  semi: true,            
-  
+  semi: true,
+
   // 문자열에 작은따옴표 사용
-  singleQuote: true,     
+  singleQuote: true,
 
   // ES5에 맞춰 배열/객체 끝에 쉼표 추가
-  trailingComma: 'es5',  
+  trailingComma: 'es5',
 
   // 줄 바꿈 기준 길이 (가독성 확보)
-  printWidth: 100,       
+  printWidth: 100,
 
   // 들여쓰기 너비
-  tabWidth: 2,           
+  tabWidth: 2,
 
   // 단일 매개변수 화살표 함수는 괄호 생략
-  arrowParens: 'avoid'   
+  arrowParens: 'avoid',
 };
+
+export default config;
