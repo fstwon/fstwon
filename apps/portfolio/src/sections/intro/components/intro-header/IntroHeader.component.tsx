c446716c9e8b1411f7c62f425b299688520c1b50
fstwon <fstwon@gmail.com>
2025-10-22 23:11:12 +0900

    chore: 코딩 컨벤션 정리

--- apps/portfolio/src/sections/intro/components/intro-header/IntroHeader.component.tsx
+++ apps/portfolio/src/sections/intro/components/intro-header/IntroHeader.component.tsx
@@ -1,23 +1,26 @@
 import './introHeader.styles.scss';
 import { WebIcon } from '@/assets/icons';
 import { useResponseLayout } from '@fstwon/utils/react';
+import { Activity } from 'react';
 
 function IntroHeader() {
 	const { isMobile, isTablet, isDesktop } = useResponseLayout();
 
 	return (
-		<section className='intro__header'>
-			<div className='intro__description'>
-				Web Developer
-				<WebIcon className='intro__header__icon-web' />
-			</div>
-			<p className='intro__description'>
-				Building modern, responsive web experiences
-			</p>
-			<p className='intro__description'>
-				with React.js and semantic web standards.
-			</p>
-		</section>
+		<Activity>
+			<section className='intro__header'>
+				<div className='intro__description'>
+					Web Developer
+					<WebIcon className='intro__header__icon-web' />
+				</div>
+				<p className='intro__description'>
+					Building modern, responsive web experiences
+				</p>
+				<p className='intro__description'>
+					with React.js and semantic web standards.
+				</p>
+			</section>
+		</Activity>
 	);
 }
 
