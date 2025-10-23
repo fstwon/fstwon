04999f13b15ee0d0461bea3ebf6db230b92f0be8
fstwon <fstwon@gmail.com>
2025-10-23 23:30:44 +0900

    feat: gsap 이거 저거 해보는 중

--- apps/portfolio/src/sections/intro/components/intro-logo-container/IntroLogoContainer.component.tsx
+++ apps/portfolio/src/sections/intro/components/intro-logo-container/IntroLogoContainer.component.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import './introLogoContainer.styles.scss';
 import {
 	MainLogo,
@@ -8,10 +9,55 @@ import {
 	OracleLogo,
 	SpringLogo,
 } from '@/assets/logos';
+import gsap from 'gsap';
 
 function IntroLogoContainer() {
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (containerRef.current) {
+			const logos = containerRef.current.querySelectorAll('.intro__logo');
+			const mainLogo = containerRef.current.querySelector('.intro__logo-main');
+			const gsapTl = gsap.timeline();
+
+			gsapTl.fromTo(
+				logos,
+				{
+					opacity: 0,
+					y: -100,
+				},
+				{
+					opacity: 0.5,
+					y: 0,
+					duration: 2,
+					ease: 'elastic',
+					delay: 0.5,
+					stagger: 0.1,
+				}
+			);
+
+			gsap.fromTo(
+				mainLogo,
+				{
+					opacity: 0,
+					y: 100,
+				},
+				{
+					opacity: 1,
+					y: 0,
+					duration: 0.5,
+					delay: 1.6,
+					ease: 'power2.inOut',
+				}
+			);
+		}
+	}, []);
+
 	return (
-		<div className='intro__logo__container relative w-full h-[45vw]'>
+		<div
+			ref={containerRef}
+			className='intro__logo__container relative w-full h-[45vw]'
+		>
 			<MainLogo className='intro__logo-main w-[85%]' />
 			<HtmlLogo className='intro__logo html w-[8%]' />
 			<Css3Logo className='intro__logo css w-[8%]' />
