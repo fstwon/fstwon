8c4365bf13b4fa2240b71ff8ba6cd0beeaf58716
fstwon <fstwon@gmail.com>
2025-10-29 23:43:19 +0900

    feat: works section UI 구현 준비

--- apps/portfolio/src/app/App.tsx
+++ apps/portfolio/src/app/App.tsx
@@ -1,9 +1,11 @@
 import { IntroSection } from '@/sections/intro';
+import { WorksSection } from '@/sections/works';
 
 export default function App() {
 	return (
 		<main className='app'>
 			<IntroSection />
+			<WorksSection />
 		</main>
 	);
 }
---
+++ apps/portfolio/src/sections/works/index.tsx
@@ -0,0 +1,12 @@
+import './index.scss';
+
+export function WorksSection() {
+	return (
+		<section className='works__section p-8'>
+			<h1 className='works__section__title b-'>WORKS</h1>
+			<section className='works__section__content'>
+				{/* TODO: 경력 사항 컴포넌트 구현 후 추가 */}
+			</section>
+		</section>
+	);
+}
