d2c1a6af3f3952a818ec4f0b10fa6b6cad76e092
fstwon <fstwon@gmail.com>
2025-10-29 23:42:49 +0900

    chore: intro section components index.tsx 추가 관리

---
+++ apps/portfolio/src/sections/intro/components/index.tsx
@@ -0,0 +1,4 @@
+import IntroHeader from './intro-header/IntroHeader.component';
+import IntroLogoContainer from './intro-logo-container/IntroLogoContainer.component';
+
+export { IntroHeader, IntroLogoContainer };
