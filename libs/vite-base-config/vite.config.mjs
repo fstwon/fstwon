470a36733670f960eb377474d3d846d4e44dfba6
fstwon <fstwon@gmail.com>
2025-12-03 23:57:30 +0900

    chore: Montserrat 폰트 추가

--- libs/vite-base-config/vite.config.mjs
+++ libs/vite-base-config/vite.config.mjs
@@ -36,7 +36,7 @@ const createBaseConfig = () => {
               crossorigin
             />`,
 						`<link 
-              rel="stylesheet" 
+              rel="preload" 
               href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
               crossorigin
             />`,
